608f606c39c0f53a7200c68a65b64e28f98a9a97
vincent <vincent>
2018-04-21 12:53:16 +0900

    파일 출력 기능 삭제. 클립보드 붙여 넣기 추가
    변환 결과를 파일로 출력하는 기능을 삭제하는 대신에 변환 결과를 클립보드로 붙여 넣기 하는 기능을 추가했다. tkinter 모듈을 이용했다. 다만 tkinter 모듈의 클립보드 제어는 tkinter 프로그램이 실행 중일 때에만 유지된다는 단점이 있다.

--- md2html.py
+++ md2html.py
@@ -2,44 +2,46 @@
 
 from sys import argv
 from re import compile
+from tkinter import Tk
+
+def set_clipboard(html):
+    root = Tk()
+    root.clipboard_clear()
+    root.clipboard_append(html)
+    root.mainloop()
 
 def md2html(md):
-	# h2
-	p = compile('## (.*)')
-	md = p.sub('<h2>\g<1></h2><hr>', md)
+    # h2
+    p = compile('## (.*)')
+    md = p.sub('<h2>\g<1></h2><hr>', md)
 
-	# bold
-	p = compile('[*](.*?)[*]')
-	md = p.sub('<b>\g<1></b>', md)
+    # bold
+    p = compile('[*](.*?)[*]')
+    md = p.sub('<b>\g<1></b>', md)
 
-	# br
-	md = md.replace('\n', '<br>')
+    # br
+    md = md.replace('\n', '<br>')
 
-	# multi-line code
-	p = compile('```(.*?)```')
-	md = p.sub("<div style='background-color: lightgray;'>\g<1></div>", md)
+    # multi-line code
+    p = compile('```(.*?)```')
+    md = p.sub("<div style='background-color: lightgray;'>\g<1></div>", md)
 
-	# code
-	p = compile('`(.*?)`')
-	md = p.sub("<span style='background-color: lightgray;'>\g<1></span>", md)
+    # code
+    p = compile('`(.*?)`')
+    md = p.sub("<span style='background-color: lightgray;'>\g<1></span>", md)
 
-	return md
+    return md
 
 if __name__ == '__main__':
 
-	if len(argv) != 2:
-		print('usage : html2md [filename]')
-		exit()
+    if len(argv) != 2:
+        print('usage : html2md [filename]')
+        exit()
 
-	f = open(argv[1])
-	md = f.read()
-	f.close()
+    f = open(argv[1])
+    md = f.read()
+    f.close()
 
-	html = md2html(md)
-	
-	# create html file
-	f = open('result.html', 'w')
-	f.write(html)
-	f.close()
+    html = md2html(md)
 
-	print('[*] html file is created.')
+    set_clipboard(html)
